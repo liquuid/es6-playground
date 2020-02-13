e510d86623f0dc781ae0edb7c77e81146b8a1cea
Fernando Henrique <liquuid@gmail.com>
2020-02-13 16:34:55 -0300

    adding repo

--- src/main.js
+++ src/main.js
@@ -1,12 +1,52 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+        this.registerHandlers();
 
-class Api {
-    static async getUserInfo(username) {
-        const response = await axios.get(`https://api.github.com/users/${username}`);
-        
-    } catch (err) {
-        console.warn("Erro na api");
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        // não deixa o form recarregar a página ou enviar o form
+        event.preventDefault();
+        this.repositories.push({
+            name: 'quebrada digital',
+            description: 'De um up na sua vida',
+            avatar_url: 'https://avatars0.githubusercontent.com/b',
+            html_url: 'http://github.com/liquuid'
+        });
+        this.render();
+    }
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
     }
 }
 
-Api.getUserInfo('liquuid');
+new App();
