0fb0e97dae5e21f933dee3912d0a7afc28a9d70c
Fernando Henrique <liquuid@gmail.com>
2020-02-13 14:59:28 -0300

    initial

---
+++ main.js
@@ -0,0 +1,29 @@
+class List {
+    constructor() {
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+
+class TodoList extends List {
+    constructor(){
+        super();
+        this.usuario = "User1";
+    }
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('novo todo');
+
+}
+MinhaLista.mostraUsuario();
